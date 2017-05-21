673e2062c35cfb40644cac0e14ce9e191c8fead3
Stas Korobeynikov <stskrbnkv@gmail.com>
2017-05-21 15:03:14 +0300

    add hello world seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\HelloWorld;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(HelloWorldTableSeeder::class);
+    }
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(array(
+            'name' => 'firstuser',
+            'email' => 'firstuser@email.com',
+            'password' => Hash::make('first_password')
+        ));
+
+        User::create(array(
+            'name' => 'seconduser',
+            'email' => 'seconduser@email.com',
+            'password' => Hash::make('second_password')
+        ));
+    }
+}
+
+class HelloWorldTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('hello_world')->delete();
+
+        HelloWorld::create(array(
+            'hello' => 'Hi',
+            'world' => 'England',
+        ));
+
+        HelloWorld::create(array(
+            'hello' => 'Salut',
+            'world' => 'France',
+        ));
     }
 }
