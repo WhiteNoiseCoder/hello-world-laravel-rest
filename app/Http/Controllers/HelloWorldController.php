b85d1d1dc18ea5a1abdff9007d9454c5f0586681
Stas Korobeynikov <stskrbnkv@gmail.com>
2017-05-21 15:30:06 +0300

    add index hello world

--- app/Http/Controllers/HelloWorldController.php
+++ app/Http/Controllers/HelloWorldController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\HelloWorld;
 
 class HelloWorldController extends Controller
 {
-    //
+    public function index()
+    {
+        $hw = HelloWorld::all();
+        return $hw->toJson();
+    }
 }
