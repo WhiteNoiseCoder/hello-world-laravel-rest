ca0c5963f8cda9f5df2ca34b84965d620075fb7e
Stas Korobeynikov <stskrbnkv@gmail.com>
2017-05-21 17:11:06 +0300

    auth required 4 hello world

--- app/Http/Controllers/HelloWorldController.php
+++ app/Http/Controllers/HelloWorldController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\HelloWorld;
+use JWTAuth;
 
 class HelloWorldController extends Controller
 {
+    function __construct()
+    {
+        $this->middleware('jwt.auth', ['except' => ['index']]);
+    }
+
     public function index()
     {
+        JWTAuth::parseToken()->authenticate();
         $hw = HelloWorld::all();
         return $hw->toJson();
     }
